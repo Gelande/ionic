8e4bd6e013e5aeb9c194ef942cd3a1ad93a28520
Gelande <agelande@gmail.com>
2020-05-13 10:51:06 -0300

    atualizacao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public resultado: String = "Resultado";
+
+  calcular() {
+
+    if(this.alcool && this.gasolina) {
+
+      let precoAlcool = parseFloat(this.alcool);
+      let precoGas = parseFloat(this.gasolina);
+
+      let result = precoAlcool / precoGas
+
+      if(result >= 0.7) {
+
+        this.resultado = "Melhor usar gasolina."
+      }else {
+        this.resultado = "Melhor usar álcool."
+      }
+
+    }else {
+      this.resultado = "Preencha os campos corretamente!"
+    }
+  }
+
   constructor() {}
 
 }
